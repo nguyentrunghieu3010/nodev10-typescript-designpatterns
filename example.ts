a9d33556dbfcb2f6d95c3fe795da982b244ed75e
Nguyen Trung Hieu <hieunt48@fsoft.com.vn>
2017-11-11 11:02:47 +0700

    Restructure app

--- example.ts
+++
@@ -1,9 +0,0 @@
-const canadianDollar = 0.91;
-
-function roundTwo(amount) {
-    return Math.round(amount * 100) / 100;
-}
-
-let temp = canadian => roundTwo(canadian * canadianDollar);
-
-console.log(temp(10));
