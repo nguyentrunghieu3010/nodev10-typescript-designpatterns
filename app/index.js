a57783ef75d523431ec7dd4f7350759d2ac0422b
Nguyen Trung Hieu <hieunt48@fsoft.com.vn>
2017-11-22 15:27:26 +0700

    task 01

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-console.log("I am node training");
+function findTheShortestSequence(number) {
+    var myArray = [number];
+    var status = true;
+    while (status) {
+        var temp = number % 2;
+        if (number == 1) {
+            break;
+        }
+        if (temp == 0) {
+            number = number / 2;
+            myArray.push(number);
+        }
+        else if (temp == 1) {
+            number = number - 1;
+            myArray.push(number);
+        }
+    }
+    return myArray;
+}
+console.log(findTheShortestSequence(17));
--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,23 @@
-console.log("I am node training");
+function findTheShortestSequence(number) {
+    let myArray = [number];
+    let status = true;
+    while (status) {
+        let temp = number % 2;
+
+        if (number == 1) {
+            break;
+        }
+
+        if (temp == 0) {
+            number = number / 2;
+            myArray.push(number)
+        } else if (temp == 1) {
+            number = number - 1;
+            myArray.push(number);
+        }
+    }
+
+    return myArray;
+}
+
+console.log(findTheShortestSequence(17));
