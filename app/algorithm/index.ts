2edbcb205fc45ee2d4d74475aa5c0c1c5d87ebc3
Nguyen Trung Hieu <hieunt48@fsoft.com.vn>
2017-11-23 09:45:02 +0700

    Done algorithm

---
+++ app/algorithm/index.ts
@@ -0,0 +1,45 @@
+function findTheShortestSequence(number) {
+    let myArray = [number];
+    let status = true;
+    while (status) {
+        let evenNum = number % 2;
+
+        if (number == 1) {
+            break;
+        }
+
+        if (evenNum == 0) {
+            number = number / 2;
+            myArray.push(number)
+        } else if (evenNum == 1) { //Odd number
+            number = number - 1;
+            myArray.push(number);
+        }
+    }
+    return myArray;
+}
+
+function findTheClosestDistance() {
+    // let myArray = [8, 24, 3, 20, 1, 17];
+    let myArray = [7, 21, 3, 42, 3, 7];
+    let smallestArray = [];
+    myArray.sort((a, b) => a - b);
+
+    for (let i = 0; i < myArray.length; i++) {
+
+        if (myArray[i] === myArray[i + 1]) {
+            smallestArray.push(0);
+        } else if (myArray[i] - myArray[i + 1] > 0) {
+            let smallestNumber = myArray[i] - myArray[i + 1];
+            smallestArray.push(smallestNumber);
+        } else if (myArray[i] - myArray[i + 1] < 0) {
+            let smallestNumber = myArray[i + 1] - myArray[i];
+            smallestArray.push(smallestNumber);
+        }
+    }
+    console.log(Math.min(...smallestArray));
+}
+
+console.log('findTheShortestSequence', findTheShortestSequence(17));
+
+console.log(findTheClosestDistance());
