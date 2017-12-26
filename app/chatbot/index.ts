7043df85e9c55696f8aa4714143ca5b5fa8a9177
Huge Nguyen <nthieu30101985@gmail.com>
2017-12-26 16:20:05 +0700

    Chatbot

--- app/chatbot/index.ts
+++ app/chatbot/index.ts
@@ -1,7 +1,7 @@
 const apiai = require('apiai');
 const app = apiai('a733a2dae0314056b55b1c7fcfa26229');
 
-const request = app.textRequest('Hi', { sessionId: 'mybot-ea1ad' });
+const request = app.textRequest('monk', { sessionId: 'mybot-ea1ad' });
 
 request.on('response', (response) => {
     console.log('Response : ', response);
