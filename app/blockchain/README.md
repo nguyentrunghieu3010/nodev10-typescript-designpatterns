dcca1983bf4b66e0ac4d620c056bd4e5ad606e23
Huge Nguyen <nthieu30101985@gmail.com>
2018-01-02 13:31:16 +0700

    Blockchain tutorial

--- app/blockchain/README.md
+++ app/blockchain/README.md
@@ -1,1 +1,5 @@
 https://github.com/SavjeeTutorials/SavjeeCoin/blob/master/package.json
+
+https://www.youtube.com/watch?v=_160oMzblY8
+
+https://github.com/anders94/blockchain-demo
--- app/chatbot/index.ts
+++ app/chatbot/index.ts
@@ -1,7 +1,7 @@
 const apiai = require('apiai');
 const app = apiai('a733a2dae0314056b55b1c7fcfa26229');
 
-const request = app.textRequest('I am Hieu from FPT Software', { sessionId: 'mybot-ea1ad' });
+const request = app.textRequest('your name is huge nguyen ?', { sessionId: 'mybot-ea1ad' });
 
 request.on('response', (response) => {
     console.log('Response : ', response);
