ffa7b55b0b5ec2d5515f9e0b1a453cc2bae95a18
Nguyen Trung Hieu <hieu.nt1@ad.tctav.com>
2019-06-04 11:04:37 +0700

    update read me

--- src/behavioral/interpreter/index.ts
+++ src/behavioral/interpreter/index.ts
@@ -8,11 +8,9 @@ export namespace InterpreterDemo {
             let person2: InterpreterDesignPattern.Expression = new InterpreterDesignPattern.TermimalExpression('Tam');
             let isSingle: InterpreterDesignPattern.Expression = new InterpreterDesignPattern.OrExpression(person1, person2);
 
-
             let person3: InterpreterDesignPattern.Expression = new InterpreterDesignPattern.TermimalExpression('Loan');
             let person4: InterpreterDesignPattern.Expression = new InterpreterDesignPattern.TermimalExpression('Kien');
             let isCommitted: InterpreterDesignPattern.Expression = new InterpreterDesignPattern.OrExpression(person3, person4);
-
 
             console.log('Hieu isSingle ::', isSingle.interpret('Hieu'));
             console.log('Tam isSingle ::', isSingle.interpret('Tam'));
---
+++ src/behavioral/mediator/README.md
@@ -0,0 +1,18 @@
+## Problems
+
+
+## Solutions
+
+![Solutions](https://refactoring.guru/images/patterns/content/mediator/mediator.png)
+
+>  restricts direct communications between the objects and forces them to collaborate only via a mediator object
+
+## Real World
+
+![Real](https://refactoring.guru/images/patterns/diagrams/mediator/live-example.png)
+
+> Aircraft pilots don’t talk to each other directly when deciding who gets to land their plane next. All communication goes through the control tower.
+
+## Relations with Other Patterns
+
+> 
